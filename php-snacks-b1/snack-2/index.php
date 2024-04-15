b0fb3c4e9e490fb7aa84d3496dacf7e528abd282
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-15 12:45:53 +0200

    snack-2 initial commit

---
+++ php-snacks-b1/snack-2/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Con un form passare come parametri GET name, mail e age
+Verificare (cercando i metodi che non conosciamo nella documentazione):
+    -name sia più lungo di 3 caratteri
+    -mail contenga un punto e una chiocciola
+    -age sia un numero.
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+ */
